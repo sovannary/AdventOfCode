f3da51fc07deada52e3b1b05523bf41836da2b00
Sovannary Tan <59174924+sovannary@users.noreply.github.com>
2024-12-01 23:53:15 -0500

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,6 +11,10 @@ import java.util.Scanner;
 public class Day1 {
     List<Integer> list1 = new ArrayList<>();
     List<Integer> list2 = new ArrayList<>();
+    Hashtable<Integer, Integer> hash2 = new Hashtable<Integer, Integer>();
+
+    public Day1() {
+    }
 
     private void readPairsFromFile(String fileName) {
         try (Scanner scanner = new Scanner(new File(fileName))) {
@@ -46,36 +51,47 @@ public class Day1 {
             int num1 = list1.get(idx);
             int num2 = list2.get(idx);
             int distance = Math.abs(num1 - num2);
-            System.out.println(num1 + " " + num2 + " " + distance);
+            // System.out.println(num1 + " " + num2 + " " + distance);
             total += Math.abs(num1 - num2);
         }
         return total;
     }
 
-    // private Hashtable<Integer, Integer> condenseNumCount(List<Integer> list) {
-    //     Hashtable<Integer, Integer> hash = new Hashtable<Integer, Integer>();
-    //     List<Integer> tmp = new ArrayList<>();
-    //     for (int num : list) {
+    private void condenseList2() {
+        for (int num : list2) {
+            Integer count = hash2.get(num);
+            if (count == null) {
+                hash2.put(num, 1);
+            }
+            else {
+                hash2.put(num, count + 1);
+            }
+        }
 
-    //     list1 = new ArrayList<>();
+    }
 
-    // }
-    // private int similarityScore() {
-    //     int totalScore = 0;
-    //     int idx2 = 0;
-    //     for (int idx1=0; idx1 < list1.size() && idx2 < list2.size(); idx1++) {
-    //         if (list1[])
-    //     }
+    private int similarityScore() {
+        condenseList2();
+    
+        int totalScore = 0;
 
-    //     return totalScore;
-    // }
+        for (int num : list1) {
+            Integer count = hash2.get(num);
+            if (count != null) {
+                totalScore += num * count;
+            }
+        }
+
+        return totalScore;
+    }
 
     public static void main(String[] args) {
         Day1 day1 = new Day1();
-        // day1.readPairsFromFile("pairs.txt");
-        day1.readPairsFromFile("small.txt");
+        day1.readPairsFromFile("pairs.txt");
+        // day1.readPairsFromFile("small.txt");
 
         System.out.println("total distance is "+ day1.totalDistance());
+        System.out.println("total similarity score is "+ day1.similarityScore());
     }
 
 }
